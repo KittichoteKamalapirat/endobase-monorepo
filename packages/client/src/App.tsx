6987622692973ca1e6cb6568258156afc88ca4c4
shane <shane@groundedpackaging.co>
2022-10-16 00:07:26 +0700

    add react router dom v6

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import logo from "./logo.svg";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
       </header>
     </div>
   );
